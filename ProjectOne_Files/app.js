9c32981821dfe7e0fcbf59794355ec65d5a6ebab
matt-meyer-nyc <Matt@Matts-MacBook-Pro.local>
2016-03-02 19:50:33 -0500

    initial layout with images commit

---
+++ ProjectOne_Files/app.js
@@ -0,0 +1,16 @@
+window.onload = function () {
+  console.log("page loaded")
+
+
+var hillary = getElementsByClassName('hillary')
+var bernie = getElementsByClassName('bernie')
+var donkey = getElementsByClassName('donkey')
+
+
+
+
+
+
+
+
+}; //END OF FILE
