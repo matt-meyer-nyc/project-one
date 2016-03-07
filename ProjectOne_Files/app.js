c8101cb02583aa74c27cba1828a776c8f74b7ad1
matt-meyer-nyc <Matt@Matts-MacBook-Pro.local>
2016-03-06 19:40:08 -0500

    Commit: 3/4 there, layout good; only Bernie moves

--- ProjectOne_Files/app.js
+++ ProjectOne_Files/app.js
@@ -1,246 +1,233 @@
 window.onload = function () {
-  console.log("page loaded");
 
-  var bernImg = document.querySelector(".bernieimg");
-  var hillImg = document.querySelector(".hillaryimg");
+var fieldWidth = 878;      // how wide is the playing field
+var fieldHeight = 643;     // how tall is the playing field
+var fieldColor = document.querySelector('.whitehouse'); // what color is the playing field
 
+var paddleWidth = 10;      // how wide is the paddle
+var paddleHeight = 80;     // how tall is the paddle
+var paddleColor = 0x00ff00;// what color is the paddle
+var paddleSpeed = 6;      // how fast will the paddle move
+var paddleXPos = fieldWidth - Math.floor(fieldWidth / 10); // 10% from right
+var paddleYPos = Math.floor((fieldHeight - paddleHeight)/2); // middle
 
-// *************************************************
-// CITATION: Code Below Based on Skeleton From:
-//   http://www.webreference.com/programming/javascript/java_anim2/4.html
-  var dirx = 1;
-  var diry = 1;
-  var spx = 10;
-  var spdy = setRand();
+var paddleWidthHill = 10;      // how wide is the paddle
+var paddleHeightHill = 80;     // how tall is the paddle
+var paddleColorHill = 0x00ff00;// what color is the paddle
+var paddleSpeedHill = 6;      // how fast will the paddle move
+var paddleXPosHill = fieldWidth - Math.floor(fieldWidth / 10); // 10% from right
+var paddleYPosHill = Math.floor((fieldHeight - paddleHeight)/2); // middle
 
-  var imgLeftIn;
-  var imgTopInt;
-  var imgWidth;
-  var divWidth;
-  var divHeight;
+var ballSize = 40;         // how big is the ball
+var ballXPos = Math.floor(fieldWidth / 10); // start 10% from left
+var ballYPos = Math.floor((fieldHeight - ballSize) / 2); // middle
+var ballXSpeed = Math.floor(Math.random() * 3) + 1;        // what is the initial velocity of the ball
+var ballYSpeed = 2;
+var ballColor = document.querySelector("#ball");  // what color is the ball
 
-  var theBall;
+var keyState = 0;          // current state of arrow keys
+var intTimeHandle =0;         // pointer to the interval timer
 
-  var t = 0;
-
-  var rInt, lInt;
-
-  function animBall () {
-    theBall     = document.getElementById('.donkeyimg');
-    imgLeftInt  = parse(theBall.style.left);
-    imgTopInt   = parseInt(theBall.style.top);
-    imgHeighInt = parseInt(theBall.height);
-    imgWidthInt = parseInt(theBall.width);
-
-    winWidth = 878;
-    winHeight = 632;
-
-    PadLoc ();
-
-    bernR  = parseInt(bernImg.style.left);
-    hillL = parseInt(hillImg.style.left) + parseInt(hillImg.style.width);
-      if ((imgLeftInt) == bernR) {
-        bernHit ();
-      } else if ((imgLeftINt) == hillL) {
-        hillHit();
-      }
-      if (dirx == 1) {
-        goRight();
-      } else {
-        goLeft();
-      }
-      if (diry == 1) {
-        goDown();
-      } else {}
-        goUP();
-    }
-  };
-
-
-
-  ////will set y-axis to a random speed
-    function setRand () {
-      randNum = Math.floor(Math.random()*6) +2;
-      return randNum;
-    }
-
-/////should serve ball
-    function captureKey(e) {
-       if (e.keyCode) {
-            keycode=e.keyCode;  // works with the DOM
-       }
-       move=String.fromCharCode(keycode);
-          if (move == "s") {
-              serveBall();
-          }
-    }
-      //hopefully adds to DOM w/o event listener
-    document.onkeydown = captureKey;
-
-
-
-    function goRight () {
-      theBall.style.left = imgLeftInt+spdx + "px";
-      if (imgLeftInt > (winWidth = (imgWidth))) {
-        score('left');
-      }
-    }
-
-    function goRight () {
-      theBall.style.left = imgLeftInt-spdx + "px";
-      if (imgLeftInt < 10) {
-        score('right');
-      }
-    }
-
-    function goDown () {
-      theBall.style.top = imgTopInt+spdy + "px";
-      if (imgTopInt > (winHeight -(imgWidth + spdy))) {
-        diry = 0;
-        spdy = setRand();
-      }
-    }
-
-    function goUp () {
-      theBall.style.top = (imgLeftInt-spdy) + "px";
-      if (imgLeftInt < 5) {
-        diry = 1;
-        spdy = setRand();
-      }
-    }
-
-    function PadLoc() {
-      bernImg;
-      rInt = parseInt(bernImg.style.top);
-      hillImg;
-      lInt = parseInt(hillImg.style.top);
-      return bernImg, rIng, hillImg, lInt;
-    }
-
-    function bernHit () {
-    rBottom = rInt + parseInt(bernImg.style.height);
-    rTop = rInt - imgHeight;
-    if ((imgTopInt > rTop)) {
-
-    dirx = 0;
-  }
-}
-
-function hillHit () {
-  lBottom = lInt + parseInt (hillImg.style.height);
-  lTop = lInt - imgHeight;
-  if ((imgTopInt < lTop)) {
-    dirx = 1;
-  }
-}
-
-function score (side) {
-  clearInterval (t);
-  msgBox = document.getElementById('messageBox')
+// Debug output
+// you can send debug print message to the screen using this function
+// this is helpful if you are not getting the results that you expect
+function output(message) {
+    document.getElementById("output").innerHTML += message + "<br/>\n";
 }
 
 
 
-// // ***************************************************
-// // //CITATION: Code below provided by Matt Short at one-on-one meeting
+function setPosition(x,y) {
+    this.xpos = x;
+    this.ypos = y;
+    this.obj.style.top = y.toString() + "px";
+    this.obj.style.left = x.toString() + "px";
+}
+
+function setColor(newColor) {
+    newColor = "000000" + newColor.toString(16);
+    this.obj.style.backgroundColor = "#" + newColor.substr(newColor.length - 6);
+}
+
+// Playing field object
+function PlayingField(width, height, color) {
+    this.width = width;
+    this.height = height;
+    this.obj = document.getElementById("playingField");
+    this.obj.style.width = width.toString() + "px";
+    this.obj.style.height = height.toString() + "px";
+    this.setColor = setColor;
+    this.setColor(color);
+    this.obj.onclick = function() {
+        // start the game when the field is clicked
+        // setup the timer tick event handler
+        document.getElementById('instructions').style.display = 'none';
+        if (intTimeHandle === 0) {
+            intTimeHandle = setInterval(function(){timerTick();},10);
+        }
+    };
+}
+
+// Paddle object
+function Paddle(width, height, color, speed, xpos, ypos) {
+    this.width = width;
+    this.height = height;
+    this.speed = speed;
+    this.obj = document.getElementById("paddle");
+    this.obj.style.width = width.toString() + "px";
+    this.obj.style.height = height.toString() + "px";
+
+    // you can use this method to change the color of the paddle
+    this.setColor = setColor;
+    this.setColor(color);
+
+    // you can use this method to change the location of the paddle
+    this.setPosition = setPosition;
+    this.setPosition(xpos,ypos);
+}
+
+// Ball object
+function Ball(size, xpos, ypos, xspeed, yspeed, color) {
+    this.xspeed = xspeed;
+    this.yspeed = yspeed;
+
+    this.obj = document.getElementById("ball");
+
+    // you can use this method to update the ball's location
+    this.setPosition = setPosition;
+
+    // you can use this method to change the size of the ball
+    this.setSize = setSize;
+    function setSize(size) {
+        this.size = size;
+        this.obj.style.width = this.size.toString() + "px";
+        this.obj.style.height = this.size.toString() + "px";
+        var radius = Math.floor(size / 2);
+        this.obj.style.borderRadius = radius.toString() + "px";
+        this.obj.style.MozBorderRadius = radius.toString() + "px";
+        this.obj.style.WebKitBorderRadius = radius.toString() + "px";
+    }
+
+    this.setPosition(xpos,ypos);
+    this.setSize(size);
+    this.setColor = setColor;
+    this.setColor(color);
+}
+
+// instantiate a field object - this is the background object
+var field = new PlayingField(fieldWidth, fieldHeight, fieldColor);
+
+// instantiate a paddle object - this is what the player controls
+var paddle = new Paddle(paddleWidth, paddleHeight, paddleColor, paddleSpeed, paddleXPos, paddleYPos);
+
+// instantiate a ball object - the bouncing ball
+var ball = new Ball(ballSize, ballXPos, ballYPos, ballXSpeed, ballYSpeed, ballColor);
+
+// The following functions track the arrow keys when they are pressed
+// pressing the up arrow key will cause keyState to be -1
+// (negative is the up direction)
+// pressing the down arrow key will cause keyState to be 1
+// (positive is the down direction)
+// releasing either key will cause keyState to be 0
+// (no direction - stop the motion of the paddle)
+document.onkeydown = function (e) {
+    var evt = e || window.event;
+    if (evt.keyCode == 38) {
+        // up arrow
+        keyState = -1;
+    } else if (evt.keyCode == 40) {
+        // down arrow
+        keyState = 1;
+    }
+    return false;
+};
+
+document.onkeyup = function (e) {
+    var evt = e || window.event;
+    if (evt.keyCode == 38 || evt.keyCode == 40) {
+        keyState = 0;
+    }
+    return false;
+};
+////////////////////////////////////////////////////
+
+//////////////////////////////////////////////////////
 
 
-// //   var bernie = {
-// //     y: 50,
-// //     el: document.querySelector('.bernie')/*,
-// //     redraw: function () {
-// //
-// //      },
-// //      update: function () {
-// //
-// //     } */
-// //   };
-// //
-// //   var hillary = {
-// //     y: 50,
-// //     el: document.querySelector('#hillary')/*,
-// //     redraw: function () {
-// //
-// //      },
-// //      update: function () {
-// //
-// //     } */
-// //   };
-// //
-// //   var donkey = {
-// //    x: 50,
-// //    y: 50,
-// //    el: document.querySelector('donkey'),
-// //    tick: function () {
-// //      // update current x + y values
-// //    },
-// //    redraw: function () {
-// //      // update this.el to reflect new x + y values
-// //    },
-// //    update: function () {
-// //      this.tick();
-// //      this.checkForCollision();
-// //      this.redraw();
-// //    },
-// //    checkForCollision: function () {
-// //      if (this.x >= bernie.x) {
-// //
-// //      } else if (this.x <= hillary.x) {
-// //
-// //      } else {
-// //
-// //      }
-// //    },
-// //    players: [
-// //      bernie,
-// //      hillary
-// //    ]
-// //  };
-// //
-// //   // game = {
-// //   //  tick: function () {
-// //   //    hillary.tick();
-// //   //    bernie.tick();
-// //   //    donkey.tick();
-// //   //  }
-// //   // }
-// **********************************************
 
 
 
-// **************************************************************************************************************************************
-//   // CITATION: BELOW CODE BASED OFF - How to move a character on a screen with Javascript (https://www.youtube.com/watch?v=vbnjxXCq5HQ)
+//////////////////////////////////////////////////////
 
-var hillBottom = 600;
-var bernBottom = 705;
+// This function should be called when the ball contacts the right wall
+// of the field. This should only happen when the player fails to contact
+// the ball with the paddle.
+function gameOver() {
+    clearInterval(intTimeHandle);
+    document.getElementById('instructions').innerHTML = "Game Over";
+    document.getElementById('instructions').style.display = "block";
+    field.setColor(0x033003);
+}
 
-function anim (e) {
-  if (e.keyCode == 87) {
-    hillBottom += 30;
-    hillImg.style.bottom = hillBottom + "px";
-  }
-  if (e.keyCode == 90) {
-    hillBottom -= 30;
-    hillImg.style.bottom = hillBottom + "px";
+// create a timer call back function
+// this is a list of things that need to be done every time the timer ticks
+function timerTick() {
+    // ******************** put your code here ***************************
 
-  } if (e.keyCode == 73) {
-    bernBottom += 30;
-    bernImg.style.bottom = bernBottom + "px";
-  }
-  if (e.keyCode == 77) {
-    bernBottom -= 30;
-    bernImg.style.bottom = bernBottom + "px";
-  }
+    // Adjust the ball position
+    var currx = ball.xpos;
+    var curry = ball.ypos;
+    var prevx = ball.xpos;
 
-  }
+    currx += ball.xspeed;
+    curry += ball.yspeed;
 
-document.onkeydown = anim;
-// *****************************************************
+    // Check for ball leaving field
+    // look for hit on left wall
+    if (currx < 0) {
+        currx = 0;
+        ball.xspeed *= -1;
+    }
+    // look for hit on top wall
+    if (curry < 0) {
+        curry = 0;
+        ball.yspeed *= -1;
+    }
+    // look for hit on right wall
+    if (currx > (field.width - ball.size)) {
+        currx = (field.width - ball.size);
+        //ball.xspeed *= -1;
+        gameOver();
+    }
+    // look for hit on bottom wall
+    if (curry > (field.height - ball.size)) {
+        curry = (field.height - ball.size);
+        ball.yspeed *= -1;
+    }
+
+    // check for ball contacting paddle
+    if ((prevx <= (paddle.xpos - ball.size)) && (currx > (paddle.xpos - ball.size))) {
+        if ((curry > (paddle.ypos - Math.floor(ball.size/2))) && (curry < paddle.ypos + paddle.height - Math.floor(ball.size/2))) {
+            ball.xspeed *= -1.1;
+        }
+    }
+    ball.setPosition(currx,curry);
+
+    // Adjust the paddle position
+    var pady = paddle.ypos;
+    pady = pady + (paddle.speed * keyState);
+    if (pady < 0) {
+        pady = 0;
+        keyState = 0;
+    }
+    if (pady > (field.height - paddle.height)) {
+        pady = field.height - paddle.height;
+        keyState = 0;
+    }
+    paddle.setPosition(paddle.xpos, pady);
+}
 
 
+};
 
-
-
-
-
-}; //END OF FILE
+//END PROGRAM
